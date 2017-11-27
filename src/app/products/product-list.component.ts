2776be8d92d9456d7823d9ef11a09f7da845e808
snowyfrozy <snowyfrozy@gmail.com>
2017-11-27 23:45:58 +0100

    Add functionality to the show/hide button

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] = [
     {
       "productId": 1,
@@ -28,4 +31,8 @@ export class ProductListComponent {
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
     }
   ];
+
+  toogleImage(): void{
+    this.showImage = !this.showImage;
+  }
 }
