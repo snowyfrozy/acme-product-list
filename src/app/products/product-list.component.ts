4801c6a8a7c050cbb6b42b485e23e49566f8bf9b
Jovial Contriver <snowyfrozy@gmail.com>
2017-11-29 19:17:49 -0800

    Updated to chapter 7

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,38 +1,72 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
-  selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
-  pageTitle: string = 'Product List';
-  imageWidth: number = 50;
-  imageMargin: number = 2;
-  showImage: boolean = false;
-  products: any[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2016",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }
-  ];
+export class ProductListComponent implements OnInit {
 
-  toogleImage(): void{
-    this.showImage = !this.showImage;
-  }
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
+
+    }
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2016",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+        }
+    ];
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = '';
+    }
+
+    ngOnInit(): void {
+        console.log("In OnInit");
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    // Emited from the StarComponent via the HTML
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List ' + message;
+    }
 }
